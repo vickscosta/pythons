d1590db10d4d618a444bfc3c8d8010f410fc7d1a
costav <victor.costa@ifp.fr>
2020-11-15 17:43:35 +0100

    other virus

--- Divers/Game_of_life.py
+++ Divers/Game_of_life.py
@@ -34,27 +34,38 @@ nb_cells=matrix_height*matrix_height
 
 matrix=np.zeros(shape=(matrix_height,matrix_lenght),dtype=int)
 
-init_ratio=0.6
+init_ratio=0.5
 
 for idx in range(int(init_ratio*nb_cells)):
     matrix[random.randint(0,matrix_height-1)][random.randint(0,matrix_lenght-1)]=1
 
 rect=pygame.Surface((8,8))
 rect.set_colorkey(WHITE)
-rect.fill(ORANGE)
+# rect.fill(ORANGE)
 
-def matrix_draw(m_h,m_l,matrix,rect):
-    for h in range(m_h-1):
-        for l in range(m_l-1):
+def matrix_draw(m_h,m_l,matrix,rect,color):
+    for h in range(m_h):
+        for l in range(m_l):
             if matrix[h][l]==1:
                 rect_=rect.get_rect()
                 rect_.center=(h*8+4,l*8+4)
+                rect.fill(color)
+                screen.blit(rect,rect_)
+            elif matrix[h][l]==8:
+                rect_=rect.get_rect()
+                rect_.center=(h*8+4,l*8+4)
+                rect.fill(ORANGE)
+                screen.blit(rect,rect_)
+            elif matrix[h][l]==4:
+                rect_=rect.get_rect()
+                rect_.center=(h*8+4,l*8+4)
+                rect.fill(BLACK)
                 screen.blit(rect,rect_)
 
 def count_n(m_h,m_l,matrix):
     n_matrix=np.zeros(shape=(m_h,m_l),dtype=int)
     for h in range(m_h):
-        for l in range(m_l-1):
+        for l in range(m_l):
             #up
             if h-1>=0:
                 if matrix[h-1][l]==1:
@@ -96,6 +107,29 @@ def update_matrix(m_h,m_l,matrix,n_matrix):
                     matrix[h][l]=1
                 else:
                     matrix[h][l]=0
+            
+            #zombie
+            elif matrix[h][l]==8 or matrix[h][l]==4:
+                try:
+                    if matrix[h+1][l]==1:
+                        matrix[h+1][l]=4
+                except: 
+                    pass
+                try:
+                    if matrix[h-1][l]==1:
+                        matrix[h-1][l]=4
+                except: 
+                    pass
+                try:
+                    if matrix[h][l+1]==1:
+                        matrix[h][l+1]=4
+                except: 
+                    pass
+                try:
+                    if matrix[h][l-1]==1:
+                        matrix[h][l-1]=4
+                except: 
+                    pass
             #dead
             else:
                 if n_matrix[h][l]==3: #3
@@ -104,21 +138,33 @@ def update_matrix(m_h,m_l,matrix,n_matrix):
                     matrix[h][l]=0
     return matrix
 
-matrix_draw(matrix_height,matrix_lenght,matrix,rect)
+def feed_system(matrix,size,val):
+
+    alea_x=random.randint(0,108)
+    alea_y=random.randint(0,80)
+
+    for idx_x in range(size):
+        for idx_y in range(size):
+            matrix[alea_x+idx_x][alea_y+idx_y]=val
+
+    return matrix
+
+
+matrix_draw(matrix_height,matrix_lenght,matrix,rect,GREEN)
 
 #main loop
 while running:
    
     pygame.display.flip()
     
-    clock.tick(24)
+    clock.tick(12)
     screen.fill(BLUE)
 
     n_matrix=count_n(matrix_height,matrix_lenght,matrix)
 
     matrix=update_matrix(matrix_height,matrix_lenght,matrix,n_matrix)
 
-    matrix_draw(matrix_height,matrix_lenght,matrix,rect)
+    matrix_draw(matrix_height,matrix_lenght,matrix,rect,GREEN)
 
     key = pygame.key.get_pressed()
 
@@ -126,5 +172,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == KEYDOWN:
+            if event.key==K_SPACE:
+                matrix=feed_system(matrix,10,1)
+            if event.key==K_k:
+                matrix=feed_system(matrix,1,8)
+            if event.key==K_n:
+                matrix=np.zeros(shape=(matrix_height,matrix_lenght),dtype=int)
+                for idx in range(int(init_ratio*nb_cells)):
+                    matrix[random.randint(0,matrix_height-1)][random.randint(0,matrix_lenght-1)]=1
             if event.key == K_ESCAPE:
                 running = False
